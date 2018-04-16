81312954f60ceaa3258c75148025212b6fbb4a06
Kyostar1112 <34302253+Kyostar1112@users.noreply.github.com>
2018-04-16 13:33:12 +0900

    とりあえず動いた
    かなり怪しめ

--- マルチスレッド/SourceCode/Main.h
+++ マルチスレッド/SourceCode/Main.h
@@ -1,11 +1,15 @@
+//インクルードガード.
 #pragma once
 
+//インクルード.
 #include<iostream>
 #include<memory>
 #include <thread>
 #include <vector>
 #include <Windows.h>
+#include <atomic>
 
+using namespace std;
 class clsMain
 {
 public:
@@ -13,10 +17,37 @@ public:
 	~clsMain();
 
 	void cMain();
-	void Work1();
+
+	static void Work1();
+	static void Work2();
+	static void Work3();
+	static void Work4();
+
+	int Cnt;
+#if 0
+	static void AddAtomNum()
+	{
+		m_AtomNum++;
+	}
+
+	static void SubAtomNum()
+	{
+		m_AtomNum--;
+	}
+
+	static void AddAtomNum(int Num)
+	{
+		m_AtomNum = Num;
+	}
+
+	atomic<int> GetAtomNum()
+	{
+		return m_AtomNum;
+	}
+
+#endif // 0
 
 private:
-
 };
 
 std::unique_ptr<clsMain> m_smpMain;
